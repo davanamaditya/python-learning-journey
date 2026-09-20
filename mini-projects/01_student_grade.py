c8f49c45af0c7ff1904d496f7be85287ab9d3a61
davanamaditya <adityad6614198@gmail.com>
2026-09-20 23:47:43 +0530

    Update 01_student_grade.py

---
+++ mini-projects/01_student_grade.py
@@ -0,0 +1,15 @@
+name = "Aditya"
+marks = 85
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "D"
+
+print("Student:", name)
+print("Marks:", marks)
+print("Grade:", grade)
